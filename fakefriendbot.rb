947b236c9618f9e58ee60b4ee564d8604b4e1f2d
Andrew Seward <awswrd@gmail.com>
2014-02-24 00:17:04 -0800

    fiendbot identifies w/ nickserv

---
+++ fakefriendbot.rb
@@ -0,0 +1,23 @@
+require 'cinch'
+
+my_name = "fakefriendbot"
+server_host = "irc.freenode.org"
+channel_name = "#fiendboyz.test"
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = server_host
+    c.channels = [channel_name]
+    c.nick = my_name
+    c.user = my_name
+  end
+
+  on :message, /^!say/ do |m|
+    this_channel = Channel(channel_name)
+
+    reply_message = m.message.split[1..-1].join(' ')
+    this_channel.msg reply_message
+  end
+end
+
+bot.start
--- fiendbot.rb
+++ fiendbot.rb
@@ -1,15 +1,35 @@
 require 'cinch'
+require 'cinch/plugins/identify'
+
+my_name = "fiendbot"
+nemesis_name = "fakefriendbot"
+server_hostname = "irc.freenode.org"
+channel_name = "#fiendboyz.test"
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.server = "irc.freenode.org"
-    c.channels = ["#fiendboyz.test"]
-    c.nick = "fiendbot"
-    c.user = "fiendbot"
+    c.server = server_hostname
+    c.channels = [channel_name]
+    c.nick = my_name
+    c.user = my_name
+    c.plugins.plugins = [Cinch::Plugins::Identify]
+    c.plugins.options[Cinch::Plugins::Identify] = {
+      :username => "fiendbot",
+      :password => "regularsp0ol",
+      :type     => :nickserv
+    }
   end
 
-  on :message, /fiendbot/ do |m|
-    m.reply "Hello #{m.user.nick}!"
+  on :message, /^#{my_name}/ do |m|
+    m.reply "leave me alone"
+  end
+
+  on :message, /infinite loop/ do |m|
+    nemesis = User(nemesis_name)
+    sleep 1
+    m.reply "Hey #{nemesis.nick}! Did somebody say \"infinite loop\"?!"
+    sleep 1
+    nemesis.send "!say Yes, #{my_name}! Somebody did say \"infinite loop\"!!"
   end
 end
 
