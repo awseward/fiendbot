2feeb27631c60f97900a255fc7052cacc78573f7
Andrew Seward <awswrd@gmail.com>
2014-02-23 19:21:57 -0800

    fiendbot hello world of sorts

---
+++ fiendbot.rb
@@ -0,0 +1,16 @@
+require 'cinch'
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = "irc.freenode.org"
+    c.channels = ["#fiendboyz"]
+    c.nick = "fiendbot"
+    c.user = "fiendbot"
+  end
+
+  on :message, /fiendbot/ do |m|
+    m.reply "Hello #{m.user.nick}!"
+  end
+end
+
+bot.start
